0d3b1fc5096b5fc9341d948939e2469079a1c0ce
Jerry <jerrygcgzz@gmail.com>
2022-02-16 17:43:10 -0600

    Actividad 5 de Computacíon en Java

---
+++ actividad_5.java
@@ -0,0 +1,33 @@
+public class actividad 5 {
+
+    public static void main(String[]args){
+        double X = 3;
+        double Y = 2;
+
+        //Muestra valores de X y Y
+        System.out.println("X = " + Y);
+        System.out.println("Y = " + X);
+
+        //Suma
+        double suma = X + Y;
+        System.out.println("Suma: " +suma);
+
+        //Resta
+        double resta = X - Y;
+        System.out.println("Resta: " +resta);
+
+
+        //Multipliacion
+        double multiplicacion = X * Y;
+        System.out.println("Multiplicación: " +multiplicacion);
+
+        //Division
+        double division = X / Y;
+        System.out.println("División: " +division);
+
+        //Modulo
+        double modulo = X % Y;
+        System.out.println("Módulo: " +modulo);
+
+    }
+}
---
+++ src/Actividad5.java
@@ -0,0 +1,35 @@
+public class Actividad5 {
+
+    public static void main(String[]args){
+
+        double X = 3;
+        double Y = 2;
+
+        //Muestra valores de X y Y
+        System.out.println("X = " + X);
+        System.out.println("Y = " + Y);
+
+        //Suma
+        double suma = X + Y;
+        System.out.println("Suma: " +suma);
+
+        //Resta
+        double resta = X - Y;
+        System.out.println("Resta: " +resta);
+
+        //Multiplicacion
+        double multiplicacion = X * Y;
+        System.out.println("Multiplicación: " +multiplicacion);
+
+        //Division
+        double division = X / Y;
+        System.out.println("División: " +division);
+
+        //Modulo
+        double modulo = X % Y;
+        System.out.println("Módulo: " +modulo);
+
+    }
+
+}
+
